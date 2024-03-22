48fe021de077afeb0bd527fb3b606ee3cbcbed3a
yash <yashchougule31@gmail.com>
2024-03-22 16:17:51 +0530

    episode3

---
+++ index.js
@@ -0,0 +1,13 @@
+// import React from "react";
+// import ReactDOM from 'react-dom';
+
+// const myElement = <h1> I Love JSX! </h1>
+
+// const root =ReactDOM.createRoot(document.getElementById("root"));
+// root.render(myElement); 
+import React from "react";
+import ReactDOM from "react-dom";
+
+const myElement = <h1>I Love JSX!</h1>;
+
+ReactDOM.render(myElement, document.getElementById("root"));
